a8e9da8408049762ea1ff4c168bff0ee8698b7ea
Phodal Huang <h@phodal.com>
2020-09-17 21:42:36 +0800

    feat: try to use chars

--- cmalloc-demo/src/main.c
+++ cmalloc-demo/src/main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "utf_string.h"
 
-int main() {
-    UtfString *string = newUtfString("hello");
+typedef struct Scanner_ {
+    int ptrs;
+} Scanner;
 
+Scanner *newScanner(char **patterns, int patternSize);
+
+Scanner *newScanner(char **patterns, int patternSize) {
+    for (int i = 0; i < patternSize; i++) {
+        UtfString *utfString = newUtfString(patterns[i]);
+        printf("%i", utfString->length);
+    }
+
+    void *scanner = malloc(sizeof(Scanner));
+    return scanner;
+}
+
+int main() {
+    char **strings = (char **) malloc(2 * sizeof(char *));
+    strings[0] = "hello";
+    strings[1] = "world";
+    Scanner *pScanner = newScanner(strings, 2);
+
+    UtfString *string = newUtfString("hello");
     printf("%s", string->string);
     printf("%i", string->length);
 }
--- cmalloc-demo/src/utf_string.c
+++ cmalloc-demo/src/utf_string.c
@@ -3,11 +3,12 @@
 //
 #include "utf_string.h"
 #include <stdlib.h>
+#include <string.h>
 
 UtfString *newUtfString(char *str) {
     UtfString *string;
     string = malloc(sizeof(UtfString));
     string->string = str;
-    string->length = sizeof(str);
+    string->length = strlen(str);
     return string;
 }
